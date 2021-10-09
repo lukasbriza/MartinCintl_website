bef42d2cbd51c65ce83db66f27e95ac9f1a3fbd2
DevCzar <lukasbriza@seznam.cz>
2021-10-09 14:14:30 +0200

    update

--- website/src/Pages/Hero.tsx
+++ website/src/Pages/Hero.tsx
@@ -1,5 +1,46 @@
 function Hero(){
 
+    return(
+        <div id="Hero">
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="100vw"
+            height="100vh"
+            style={{
+                backgroundColor: "rgba(0,0,0, 0.00)",
+            }}
+            
+            >
+                <defs>
+                    <radialGradient id="rgrad" cx="50%" cy="50%" r="50%">
+                    <stop
+                        offset="0%"
+                        style={{
+                        stopColor: "rgb(79,95,164)",
+                        stopOpacity: 1,
+                        }}
+                    />
+                    <stop
+                        offset="65%"
+                        style={{
+                        stopColor: "rgb(0,0,0)",
+                        stopOpacity: 0,
+                        }}
+                    />
+                    </radialGradient>
+                </defs>
+                <ellipse
+                    cx="70%"
+                    cy="58%"
+                    rx="100vw"
+                    ry="80vh"
+                    style={{
+                    fill: "url(#rgrad)",
+                    }}
+                />
+            </svg>
+        </div>
+    )
 }
 
 export {Hero}
