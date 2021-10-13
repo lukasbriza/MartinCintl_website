c7c37ef594db8cf87ecef3540f030da5efb65d4a
LuckyCoder <lukasbriza@seznam.cz>
2021-10-13 21:27:11 +0200

    menu update

--- website/src/Components/Menu/Menu.tsx
+++ website/src/Components/Menu/Menu.tsx
@@ -1,6 +1,27 @@
+import {useLocation} from 'react-router-dom'
+import {useEffect} from 'react'
+
+import {Logo} from '../Logo/Logo'
+import {ActualPage} from '../Actual_page/ActualPage'
+import {MenuButton} from '../Menu_button/MenuButton'
+import {LanguageMutation} from '../Language_mutations/LanguageMutation'
+
+
+
 function Menu(){
+    let location = useLocation()
+
+    useEffect(() =>{
+        console.log(location)
+    }, [location])
+
     return(
-        <></>
+        <section id="menu">
+            <Logo id="logo"/>
+            <ActualPage id="actualPage-counter" pages={["Fitness", "O mně", "Služby",]} location={location}/>
+            <LanguageMutation id="languageMutation" languages={["cs", "eng"]}/>
+            <MenuButton id="menu-button" menuProps={["O mně", "Služby","Kvalifikace","Reference","Kontakt"]} location={location}/>
+        </section>
     )
 }
 
