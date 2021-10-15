74a068e1f93fe78f01e963303220e3b16624510d
LuckyCoder <lukasbriza@seznam.cz>
2021-10-15 15:14:32 +0200

    route updates

--- website/src/App/PageRoutes.tsx
+++ website/src/App/PageRoutes.tsx
@@ -1,19 +1,33 @@
-import React from "react"
-import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import React, {useEffect} from "react"
+import {CSSTransition, TransitionGroup} from 'react-transition-group'
 import {Switch, Route, useLocation} from 'react-router-dom'
-import { useContext } from 'react';
+import {useContext} from 'react';
+import {PageContext} from '../Functions/Context'
 ////////////////////////////////////////
 //COMPONENTS//
 import {Menu} from '../Components/Menu/Menu'
+import {PageCounter} from '../Components/Page_counter/PageCounter'
 import {Hero} from '../Pages/Hero'
+import {About} from '../Pages/About'
+import {Products} from '../Pages/Products'
+import {Contact} from '../Pages/Contact'
 ////////////////////////////////////////
 
 function PageRoutes(){
+    const context:any = useContext(PageContext)
     const location = useLocation()
+
+    useEffect(() =>{
+        context.functions.setLocation(location.pathname)
+    }, [location])
 
     return(
         <React.Fragment>
             <Menu/>
+            <PageCounter 
+                pageLinks={["/","/about","/products","/contact"]} 
+                counterData={["01","02","03","04"]}
+            />
             <TransitionGroup>
                 <CSSTransition
                     timeout={2000}
@@ -22,9 +36,9 @@ function PageRoutes(){
                 >
                     <Switch location={location}>
                         <Route exact path="/" component={Hero}/>
-                        <Route path="/" />
-                        <Route path="/" />
-                        <Route path="/" />
+                        <Route exact path="/about" component={About}/>
+                        <Route path="/products" component={Products}/>
+                        <Route path="/contact" component={Contact}/>
                     </Switch>
                 </CSSTransition>
             </TransitionGroup>
