65c16f806d84d03a6e6eb30b0b80fb757a481bac
LuckyCoder <lukasbriza@seznam.cz>
2021-10-23 19:57:03 +0200

    menu slider move

--- website/src/App/PageRoutes.tsx
+++ website/src/App/PageRoutes.tsx
@@ -2,7 +2,7 @@ import React, {useEffect} from "react"
 import {CSSTransition, TransitionGroup} from 'react-transition-group'
 import {Switch, Route, useLocation} from 'react-router-dom'
 import {useContext} from 'react';
-import {PageContext} from '../Functions/Context'
+import {PageContext, AnimationContext} from '../Functions/Context'
 ////////////////////////////////////////
 //COMPONENTS//
 import {Menu} from '../Components/Menu/Menu'
@@ -12,12 +12,14 @@ import {Hero} from '../Pages/Hero'
 import {About} from '../Pages/About'
 import {Products} from '../Pages/Products'
 import {Contact} from '../Pages/Contact'
+import {MenuSlider} from '../Components/Menu_slider/MenuSlider'
 ////////////////////////////////////////
 //CONFIG//
 import {config} from './config'
 
 function PageRoutes(){
     const context:any = useContext(PageContext)
+    const contextAn:any = useContext(AnimationContext)
     const location = useLocation()
 
     useEffect(() =>{
@@ -30,6 +32,12 @@ function PageRoutes(){
             <PageCounter 
                 pageLinks={config.routesMain} 
                 counterData={config.counter}
+                show={contextAn.pageCounter}
+            />
+            <MenuSlider
+                routesAll={config.routesAll}
+                pagesAll={config.pagesAll}
+                location={location}
             />
             <TransitionGroup>
                 <CSSTransition
