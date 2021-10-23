f756a3cb7e396575f73dab0f5fa65ec604eba820
LuckyCoder <lukasbriza@seznam.cz>
2021-10-23 19:55:49 +0200

    menuSlider split

--- website/src/Components/Menu_button/MenuButton.tsx
+++ website/src/Components/Menu_button/MenuButton.tsx
@@ -1,40 +1,11 @@
-import {useRef,useContext, useEffect} from 'react'
-import {Link} from 'react-router-dom'
+import {useContext} from 'react'
 
 import {AnimationContext} from '../../Functions/Context'
-import {menuSliderOff, menuSliderOn} from '../../Functions/AnimationManager'
 
 function MenuButton(props:menuButton){
     const context:any = useContext(AnimationContext)
-    const menuSlider:any = useRef()
-
-    //ANIMATION LISTENER//
-    useEffect(() => {
-        if(context.menuSlider === true){
-            menuSliderOn(menuSlider.current)
-            context.functions.setActualPage(false)
-        }
-        if(context.menuSlider === false){
-            menuSliderOff(menuSlider.current)
-            context.functions.setActualPage(true)
-        }
-    }, [context.menuSlider])
-
-    //SLIDER PROPERTIES RENDERING//
-    const menuItems = props.pagesAll.map((menuItem:string, index:number) => {
-        return(
-            <Link 
-                to={props.routesAll[index]}
-                key={index+menuItem+"key"}
-                id={"_"+index+menuItem}
-            >
-                <p>{menuItem}</p>
-            </Link>
-        )
-    })
 
     return(
-        <>
             <div 
                 id={props.id}
                 onClick={() => {
@@ -43,19 +14,6 @@ function MenuButton(props:menuButton){
             >
                 <p className="center absolute">Menu</p>
             </div>
-            <div 
-                id="menuSlider"
-                ref={menuSlider}
-            >
-                <div id="menubox-wrapper">
-                    <div className="line" id="line1"></div>
-                    <div className="line" id="line2"></div>
-                    {menuItems}
-                    <div className="line" id="line3"></div>
-                    <div className="line" id="line4"></div>
-                </div>
-            </div>
-        </>
     )
 }
 
---
+++ website/src/Components/Menu_slider/MenuSlider.tsx
@@ -0,0 +1,52 @@
+import {Link} from 'react-router-dom'
+import {useRef,useContext, useEffect} from 'react'
+
+import {AnimationContext} from '../../Functions/Context'
+import {menuSliderOff, menuSliderOn} from '../../Functions/AnimationManager'
+
+function MenuSlider(props:menuSlider){
+    const context:any = useContext(AnimationContext)
+    const menuSlider:any = useRef()
+
+    //ANIMATION LISTENER//
+    useEffect(() => {
+        if(context.menuSlider === true){
+            menuSliderOn(menuSlider.current)
+            context.functions.setActualPage(false)
+        }
+        if(context.menuSlider === false){
+            menuSliderOff(menuSlider.current)
+            context.functions.setActualPage(true)
+        }
+    }, [context.menuSlider])
+
+    //SLIDER PROPERTIES RENDERING//
+    const menuItems = props.pagesAll.map((menuItem:string, index:number) => {
+        return(
+            <Link 
+                to={props.routesAll[index]}
+                key={index+menuItem+"key"}
+                id={"_"+index+menuItem}
+            >
+                <p>{menuItem}</p>
+            </Link>
+        )
+    })
+
+    return(
+        <div 
+            id="menuSlider"
+            ref={menuSlider}
+        >
+            <div id="menubox-wrapper">
+                <div className="line" id="line1"></div>
+                <div className="line" id="line2"></div>
+                {menuItems}
+                <div className="line" id="line3"></div>
+                <div className="line" id="line4"></div>
+            </div>
+        </div>
+    )
+}
+
+export {MenuSlider}
