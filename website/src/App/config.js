b7d7dd5d888b0d5eddb5495ebf4b06425bbf58fb
LuckyCoder <lukasbriza@seznam.cz>
2021-10-23 13:19:37 +0200

    config adding

---
+++ website/src/App/config.js
@@ -0,0 +1,10 @@
+const config = {
+    routesMain: ["/","/about","/products","/contact"],
+    mainPages: ["Fitness", "O mně", "Služby", "Kontakt"],
+    routesAll: ["/","/about","/products","/kvalification","/reference","/contact"],
+    pagesAll: ["Fitness","O mně", "Služby","Kvalifikace","Reference","Kontakt"],
+    counter: ["01","02","03","04"],
+    languageMutations: ["cs", "eng"]
+}
+
+export {config}
