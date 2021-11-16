dc6bc4db4468a03227034cc5720358c624718069
LuckyCoder <lukasbriza@seznam.cz>
2021-11-16 01:20:15 +0100

    dd import

--- website/src/Functions/DeviceDetect.js
+++ website/src/Functions/DeviceDetect.js
@@ -1,1 +1,12 @@
-export {}
+function deviceDetection(){
+    let isMobile;
+
+    if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+        isMobile = true;
+        return isMobile;
+    }else{
+        isMobile = false;
+        return isMobile;
+    }
+}
+export {deviceDetection}
