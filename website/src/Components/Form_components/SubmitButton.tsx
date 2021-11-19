c5c3aa210afcbc8afc7d9ba5c81b0ba51a9cf0a9
LuckyCoder <lukasbriza@seznam.cz>
2021-11-20 00:48:32 +0100

    delete files

--- website/src/Components/Form_components/SubmitButton.tsx
+++
@@ -1,7 +0,0 @@
-function SubmitButton(props:submitBtn){
-    return(
-        <button className="submitButton relative centerh" onClick={()=>{props.fn()}}>Odeslat</button>
-    )
-}
-
-export {SubmitButton}
