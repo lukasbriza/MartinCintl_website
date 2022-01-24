e5cd7265315a66cb1add57b514cd3136683622bf
LuckyCoder <lukasbriza@seznam.cz>
2022-01-24 14:18:21 +0100

    references page (construction)

---
+++ website/src/Pages/References.tsx
@@ -0,0 +1,11 @@
+function References() {
+    return (
+        <div id="References">
+            <div className="flex upperCase relative center">
+                UNDER CONSTRUCTION
+            </div>
+        </div>
+    )
+}
+
+export { References }
