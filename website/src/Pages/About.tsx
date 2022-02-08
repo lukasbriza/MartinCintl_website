335f05d04e441ebe101b5d6fcc65e10d3b3cf1e8
LuckyCoder <lukasbriza@seznam.cz>
2022-02-08 08:45:54 +0100

    correction

--- website/src/Pages/About.tsx
+++ website/src/Pages/About.tsx
@@ -3,40 +3,40 @@ import aboutImg from '../Static/AboutIMG.jpg'
 import { AnimationContext } from '../Functions/Context'
 ////////////////////////////////////////////////
 //COMPONENTS//
-import {Button} from '../Components/Button/Button'
-import {Overlay} from '../Components/Overlay/Overlay'
+import { Button } from '../Components/Button/Button'
+import { Overlay } from '../Components/Overlay/Overlay'
 
 
-function About(){
-    const context:any = useContext(AnimationContext)
+function About() {
+    const context: any = useContext(AnimationContext)
 
-    const [showModal, setShowModal]=useState(false)
+    const [showModal, setShowModal] = useState(false)
 
-    return(
-        <div 
+    return (
+        <div
             id="About"
-            style={{backgroundImage: `url(${aboutImg})`}}
+            style={{ backgroundImage: `url(${aboutImg})` }}
         >
             <div id="AboutLayer" className="wv100 hv100">
                 <section id="About-section" className="wv100">
                     <p id="mainPart">
-                        Jmenuji se MARTIN CINTL<br/>
-                        a ve <a href="https://www.tvujgym.cz/" target="_blank" rel="noreferrer">Tvůj Gym</a> působím jako trenér se specializací na <br/> 
+                        Jmenuji se MARTIN CINTL<br />
+                        a ve <a href="https://www.tvujgym.cz/" target="_blank" rel="noreferrer">Tvůj Gym</a> působím jako trenér se specializací na <br />
                         silový a nápravný trénink.
                     </p>
                     <div id="secondaryPart">
-                        Pomáhám nejen začátečníkům s osvojením techniky cvičení, ale i sportovcům zlepšit 
-                        jejich výkon a dosáhnout jejich vytyčeného cíle. V obou případech je pro mě klíčové dosáhnout co 
-                        nejlepšího výsledku za podmínky udržení celkového zdravý člověka, bez bolesti a omezení. <br/>
-                        <p id="showMore" onClick={()=>{setShowModal(true)}}>Ukaž více</p>
+                        Pomáhám nejen začátečníkům s osvojením techniky cvičení, ale i sportovcům zlepšit
+                        jejich výkon a dosáhnout jejich vytyčeného cíle. V obou případech je pro mě klíčové dosáhnout co
+                        nejlepšího výsledku za podmínky udržení celkového zdraví člověka, bez bolesti a omezení. <br />
+                        <p id="showMore" onClick={() => { setShowModal(true) }}>Ukaž více</p>
                     </div>
-                    <Button 
-                        text={"chci cvičit"} 
-                        id={"About-button"} 
-                        link={"/products"} 
-                        onClick={()=>{console.log('click')}}
-                        onMouseEnter={()=>{context.functions.setAnimationClass("Up")}}
-                        onTouchStart={()=>{context.functions.setAnimationClass("Up")}}
+                    <Button
+                        text={"chci cvičit"}
+                        id={"About-button"}
+                        link={"/products"}
+                        onClick={() => { console.log('click') }}
+                        onMouseEnter={() => { context.functions.setAnimationClass("Up") }}
+                        onTouchStart={() => { context.functions.setAnimationClass("Up") }}
                     />
                 </section>
                 <Overlay show={showModal} content={
@@ -48,10 +48,10 @@ function About(){
 
                     K tomuto povolání mne kvalifikuje více jak 8 let praxe, závodní zkušenosti (silový trojboj - kategorie RAW) 
                     a bezpočet absolvovaných odborných a rekvalifikačních kurzů.`
-                } onClick={()=>{setShowModal(false)}}/>
+                } onClick={() => { setShowModal(false) }} />
             </div>
         </div>
     )
 }
 
-export {About}
+export { About }
